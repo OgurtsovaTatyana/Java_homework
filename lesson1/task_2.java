544803553ec3947380a446970a9ba7a178d628f5
Tatyana2022 <zetaal@mail.ru>
2023-04-14 14:05:11 +0700

    отладка 2 задачи

--- lesson1/task_2.java
+++ lesson1/task_2.java
@@ -1,10 +1,11 @@
 // Вывести все простые числа от 1 до 1000
 public class task_2 {
    public static void main(String[] args) {
-   int flag=0;
+   
    System.out.println("простые числа: "); 
-   for (int num = 1; num <= 10; num++) {
-        for (int index = 1; index < num; index++) {
+   for (int num = 1; num <= 1000; num++) {
+    int flag=0;
+        for (int index = 2; index < num; index++) {
             if (num%index>0){
             flag=1;
             } else {
@@ -12,7 +13,6 @@ public class task_2 {
             break;
             }
         }
-        System.out.println("f="+flag); 
         if (flag>0) System.out.println(num);   
     
     } 
