1e977cc24087a589793d8ae1e5823147ec355841
Tatyana2022 <zetaal@mail.ru>
2023-04-14 14:00:40 +0700

    task3

---
+++ lesson1/task_3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class task_3 {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("ввдите первое число");
+    int number1=sc.nextInt();
+    System.out.println("ввдите знак");
+    String simbol=sc.next();
+    System.out.println("ввдите второе число");
+    int number2=sc.nextInt();
+    int res=0;
+    System.out.println(simbol);
+    if (simbol=="+") res=number1 + number2;
+    if (simbol=="-") res=number1 + number2;
+    if (simbol=="*") res=number1 + number2;
+    if (simbol=="/") res=number1 + number2;
+    System.out.println(res);
+}
+}
+
