64ad90f5ba339f21d28a37f66d5824f3beacee86
Tatyana2022 <zetaal@mail.ru>
2023-04-14 21:24:57 +0700

    задача1:пузырек

---
+++ lesson2/task_1.java
@@ -0,0 +1,46 @@
+package lesson2;
+import java.util.Arrays;
+import java.io.FileWriter;
+import java.io.IOException;
+public class task_1 {
+    public static void main(String[] args) {
+        int [] arr=new int[] {6,9,4,8,3,1,6,7,5,2};
+        f_sort(arr);  
+    }
+
+static void f_write(String item){
+    try(FileWriter writer= new FileWriter("longir.txt",true))
+    {
+        writer.write(item);
+        writer.close();  
+    } 
+    
+     catch (Exception ex) {
+        System.out.print(ex.getMessage()); 
+    }
+} //end static void f_write
+static void f_sort(int [] arr) {
+    int min; 
+    int count=arr.length;
+    
+    for (int index = 0; index <arr.length; index++) {
+       
+    for (int j = 1; j< count; j++) {
+        if (arr[j]<arr[j-1]) {
+            min=arr[j-1];
+            arr[j-1]=arr[j];
+            arr[j]=min;
+            
+
+        }
+        for (int item: arr) {
+        System.out.printf("%d ",item);
+        }   
+        System.out.printf(" \n");
+        String stroka=Arrays.toString(arr)+"\n";
+        f_write(stroka);
+    }
+    count=count-1;
+    }   
+} //end static void f_sort
+} //end public class task_1
