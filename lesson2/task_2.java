1c1745c2202aa63adeed533b610d8eea25e4aaec
Tatyana2022 <zetaal@mail.ru>
2023-04-15 00:15:36 +0700

    задача2:чтение из файла +Builder

---
+++ lesson2/task_2.java
@@ -0,0 +1,43 @@
+package lesson2;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class task_2 {
+ public static void main(String[] args) {
+    f_read();
+} 
+
+static void f_read(){
+    try(FileReader freader= new FileReader("lesson2/task_2.txt"))
+    {
+     BufferedReader breader = new BufferedReader(freader);
+        for (int index = 0; index < 2; index++) {        
+        String line = breader.readLine();
+        f_print(line);     
+        } 
+        breader.close();  
+    } 
+    
+     catch (Exception ex) {
+        System.out.print(ex.getMessage()); 
+    }
+} //end static void f_read 
+
+static void f_print(String line){
+    String [] words=line.split(",");
+    String[] print_word =new String [3];
+    int i=0;
+    StringBuilder result = new StringBuilder();
+    for(String word : words){
+        String[] print_words=new String[]{" получил "," по предмету ",""};        
+        String[] word_item=word.split(":");
+        StringBuilder builder = new StringBuilder(word_item[1]);
+        builder.deleteCharAt(0);
+        builder.deleteCharAt( builder.length()-1);
+        result.append(builder+print_words[i]);
+        i++;
+    }
+    System.out.println(result);
+ 
+}
+}
